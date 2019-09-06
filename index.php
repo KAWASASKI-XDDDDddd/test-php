1e6535b71324b7dc701bc320b04a25aec887a302
Казаченко Константин Андреевич <pocanovich2@mail.com>
2019-09-06 10:44:04 +1000

    added an index.php

--- index.php
+++ index.php
@@ -29,11 +29,11 @@ function sendMessage($chatID, $messaggio, $token) {
     return $result;
 }
 
-$token1 = "<917249100:AAHZRB4R2TAr_hP00ZEtBtEF5zFNaV0ipUM>";
-$chatid1 = "<-344984371>";
+$token1 = "917249100:AAHZRB4R2TAr_hP00ZEtBtEF5zFNaV0ipUM";
+$chatid1 = "-344984371";
 sendMessage($chatid1, $mas3, $token1);
 
-
+echo $url
 ?>
 
 </body>
