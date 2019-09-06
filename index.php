30c3dcca97fb4c1a957c0f22ca7a7ae5935326c4
Казаченко Константин Андреевич <pocanovich2@mail.com>
2019-09-06 10:27:12 +1000

    added an index.php

--- index.php
+++ index.php
@@ -6,7 +6,35 @@
 <body>
 
 <?php
-echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+#echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+
+
+
+$mas3 = htmlspecialchars($_GET["name"]);
+echo $mas3;
+/**
+function sendMessage($chatID, $messaggio, $token) {
+    echo "sending message to " . $chatID . "\n";
+
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+$token = "<917249100:AAHZRB4R2TAr_hP00ZEtBtEF5zFNaV0ipUM>";
+$chatid = "<-344984371>";
+sendMessage($chatid, $me1, $token);
+**/
+
+
 ?>
 
 </body>
